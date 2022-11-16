223b2475d0495a25fd8249e8cad14547620005c6
Steve Kinney <hello@stevekinney.net>
2022-11-16 13:24:25 -0600

    Add solution

--- src/components/application.jsx
+++ src/components/application.jsx
@@ -1,42 +1,10 @@
-import { useState } from 'react';
-import generateRandomColor from '../lib/generate-random-color';
-import ColorSwatch from './color-swatch';
 import ExpensiveComponent from './expensive-component';
-import GameInput from './game-input';
-import GameStatus from './game-status';
+import Game from './game';
 
 const Application = () => {
-  const [colorGuess, setColorGuess] = useState('');
-  const [correctAnswer, setCorrectAnswer] = useState(generateRandomColor());
-  const [hasGuessed, setHasGuessed] = useState(false);
-  const [isWinner, setIsWinner] = useState(false);
-
-  if (hasGuessed) {
-    if (correctAnswer === colorGuess) {
-      setIsWinner(true);
-    }
-  }
-
   return (
     <main className="flex flex-col gap-8 mx-auto my-8 w-96">
-      <ColorSwatch color={correctAnswer} />
-      <GameInput
-        value={colorGuess}
-        onChange={(e) => setColorGuess(e.target.value)}
-        onSubmit={() => setHasGuessed(true)}
-        disabled={hasGuessed}
-      />
-      <GameStatus isWinner={isWinner} hasGuessed={hasGuessed} />
-      <button
-        onClick={() => {
-          setCorrectAnswer(generateRandomColor());
-          setHasGuessed(false);
-          setColorGuess('');
-        }}
-        type={hasGuessed ? 'submit' : 'button'}
-      >
-        Reset Color
-      </button>
+      <Game />
       <ExpensiveComponent />
     </main>
   );
