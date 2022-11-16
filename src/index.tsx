ca2f5fe3574e8b10ac95dd954a6fa5774e87f207
Steve Kinney <hello@stevekinney.net>
2022-11-16 12:57:30 -0600

    Remove Mirage

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Application from './components/application';
 
-import { makeServer } from './api';
-
 import './index.css';
-
-const environment = process.env.NODE_ENV;
-makeServer({ environment });
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
