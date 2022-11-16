0b58d93281560436b316a634ee10fffb2d0dee2b
Steve Kinney <hello@stevekinney.net>
2022-11-16 13:24:25 -0600

    Add solution
    Actually return the component

---
+++ src/components/game.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import generateRandomColor from '../lib/generate-random-color';
+import ColorSwatch from './color-swatch';
+import GameInput from './game-input';
+import GameStatus from './game-status';
+
+const Game = () => {
+  const [colorGuess, setColorGuess] = useState('');
+  const [correctAnswer, setCorrectAnswer] = useState(generateRandomColor());
+  const [hasGuessed, setHasGuessed] = useState(false);
+  const [isWinner, setIsWinner] = useState(false);
+
+  if (hasGuessed) {
+    if (correctAnswer === colorGuess) {
+      setIsWinner(true);
+    }
+  }
+
+  return (
+    <div className="flex flex-col gap-8">
+      <ColorSwatch color={correctAnswer} />
+      <GameInput
+        value={colorGuess}
+        onChange={(e) => setColorGuess(e.target.value)}
+        onSubmit={() => setHasGuessed(true)}
+        disabled={hasGuessed}
+      />
+      <GameStatus isWinner={isWinner} hasGuessed={hasGuessed} />
+      <button
+        onClick={() => {
+          setCorrectAnswer(generateRandomColor());
+          setHasGuessed(false);
+          setColorGuess('');
+        }}
+        type={hasGuessed ? 'submit' : 'button'}
+      >
+        Reset Color
+      </button>
+    </div>
+  );
+};
+
+export default Game;
